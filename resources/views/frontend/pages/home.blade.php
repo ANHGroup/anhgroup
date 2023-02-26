98eb0e82810870cbe333ad390ee623c539b34189
tuhin cse <tuhincse20@gmail.com>
2023-02-26 17:23:37 +0600

    header,header_link,slider,home path change

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -33,7 +33,7 @@
     <div class="blog_section">
       <div class="single_blog col-md-3 slider-content">
         <a href="https://www.anhenterprise.com/">
-          <img src="{{asset('/assets/frontend/images/companies/ANH_Enterprise_Limited_Logo.png')}}" class="" alt="ANH Enterprise">
+          <img src="{{asset('assets/frontend/images/companies/ANH_Enterprise_Limited_Logo.png')}}" class="" alt="ANH Enterprise">
 
           <div class="slider-text">
             <h3>ANH Enterprise</h3>
@@ -43,7 +43,7 @@
       </div>
       <div class="single_blog col-md-3 slider-content">
         <a href="https://www.srdlbd.com/">
-          <img src="{{asset('/assets/frontend/images/companies/ANH_Limited_Logo.png')}}" class="" alt="ANH Limited">
+          <img src="{{asset('assets/frontend/images/companies/ANH_Limited_Logo.png')}}" class="" alt="ANH Limited">
           <div class="slider-text">
             <h3>ANH Limited</h3>
             <p>ANH Limited is one of the leading Warehouse Solution, Material Handling, Fabric Makeup, Dyeing & Automation Solution Companies in Bangladesh.</p>
@@ -52,7 +52,7 @@
       </div>
       <div class="single_blog col-md-3 slider-content">
         <a href="https://www.srdlbd.com/">
-          <img src="{{asset('/assets/frontend/images/companies/SRD_Limited_Logo.png')}}" class="" alt="SRD Limited">
+          <img src="{{asset('assets/frontend/images/companies/SRD_Limited_Logo.png')}}" class="" alt="SRD Limited">
           <div class="slider-text">
             <h3>SRD Limited</h3>
             <p>SRD Limited is one of the top construction company in Bangladesh, and we are working together to ensure your construction and business needs are met.</p>
@@ -60,7 +60,7 @@
         </a>
       </div> <div class="single_blog col-md-3 slider-content">
         <a href="https://www.srdlbd.com/">
-          <img src="{{asset('/assets/frontend/images/companies/SRD_Limited_Logo.png')}}" class="" alt="SRD Limited">
+          <img src="{{asset('assets/frontend/images/companies/SRD_Limited_Logo.png')}}" class="" alt="SRD Limited">
           <div class="slider-text">
             <h3>SRD Limited</h3>
             <p>SRD Limited is one of the top construction company in Bangladesh, and we are working together to ensure your construction and business needs are met.</p>
